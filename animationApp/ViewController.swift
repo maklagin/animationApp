5ce67f8c8bf00fa151291eb068caa302685dfd86
maklagin <108239343+maklagin@users.noreply.github.com>
2022-08-04 11:51:52 +0300

    Initial Commit

---
+++ animationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  animationApp
+//
+//  Created by Максим Кулагин on 04.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
